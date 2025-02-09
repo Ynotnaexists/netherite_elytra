9d4ed4d84589979c02080f88ba82ed8c1fdcb5b9
Ynotnaexists <antonycochran@gmail.com>
2025-02-09 13:14:21 +0000

    Cleaning up some code

---
+++ src/main/java/ynotnaexists/netheriteelytra/NetheriteElytraClient.java
@@ -0,0 +1,8 @@
+package ynotnaexists.netheriteelytra;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetheriteElytraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
