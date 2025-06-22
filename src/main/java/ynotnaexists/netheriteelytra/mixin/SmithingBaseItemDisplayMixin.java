728b4aadae648449be388adcda713afa17a3d9cf
Ynotnaexists <antonycochran@gmail.com>
2025-06-22 13:53:36 +0100

    1.21.6 update and SmithingBaseItemDisplayMixin fix

--- src/main/java/ynotnaexists/netheriteelytra/mixin/SmithingBaseItemDisplayMixin.java
+++ src/main/java/ynotnaexists/netheriteelytra/mixin/SmithingBaseItemDisplayMixin.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 @Mixin(SmithingTemplateItem.class)
 public class SmithingBaseItemDisplayMixin {
-    @Inject(method = "getNetheriteUpgradeEmptyBaseSlotTextures", at = @At("RETURN"), cancellable = true)
+@Inject(method = "getNetheriteUpgradeEmptyBaseSlotTextures", at = @At("RETURN"), cancellable = true)
     private static void injectElytraTexture(CallbackInfoReturnable<List<Identifier>> cir) {
         List<Identifier> modifiedList = new ArrayList<>(cir.getReturnValue());
-        modifiedList.add(Identifier.of("netheriteelytra", "item/empty_slot_netherite_elytra"));
+        modifiedList.add(Identifier.of("netheriteelytra", "elytra"));
         cir.setReturnValue(modifiedList);
     }
 }
